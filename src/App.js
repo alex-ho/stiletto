d16f825d3841e3d266b57e3fb7c81ae8bde63412
Taylor Holden <taylor.holden@bettercloud.com>
2017-10-05 10:47:25 -0400

    1 - Added flow to project and a tiny reacet component to show how it works.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+// @flow
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FlowTest from './FlowTest'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+
+      <FlowTest text={'This guy is type-checked by Flow'} />
+    </p>
+  </div>
+)
 
 export default App;
